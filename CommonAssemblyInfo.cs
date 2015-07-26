9a9fdc05c8d4ecb8da435d7c7b1521d6cba2833a
Simon Cropp <simon.cropp@gmail.com>
2015-07-26 21:21:00 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Fody.Equals")]
 [assembly: AssemblyProduct("Fody.Equals")]
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyVersion("1.4.2")]
+[assembly: AssemblyFileVersion("1.4.2")]
