51cc6ff821bee069dbffd60f604990cb39df7606
Rafal Jasica <rafaljasica@smtsoftware.com>
2016-02-17 22:00:07 +0100

    update version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Fody.Equals")]
 [assembly: AssemblyProduct("Fody.Equals")]
-[assembly: AssemblyVersion("1.4.7")]
-[assembly: AssemblyFileVersion("1.4.7")]
+[assembly: AssemblyVersion("1.4.8")]
+[assembly: AssemblyFileVersion("1.4.8")]
