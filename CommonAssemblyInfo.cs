6ccf328fe42ccc1b4539047ea51919d9b5805bf7
Simon Cropp <simon.cropp@gmail.com>
2017-06-10 19:17:32 +1000

    fix debug mode
    fixes #37

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Fody.Equals")]
 [assembly: AssemblyProduct("Fody.Equals")]
-[assembly: AssemblyVersion("1.7.0")]
-[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyVersion("1.7.1")]
+[assembly: AssemblyFileVersion("1.7.1")]
