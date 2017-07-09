e5405494a8e4f0e4d9e64b0b0f497394454c6499
Gaëtan Marrot <gaetan.marrot@gmail.com>
2017-07-09 11:27:38 +0200

    Support .NET Standard 1.0

--- Tests/MockAssemblyResolver.cs
+++ Tests/MockAssemblyResolver.cs
@@ -1,45 +1,8 @@
-using System;
-using System.CodeDom.Compiler;
-using System.IO;
-using System.Reflection;
 using Mono.Cecil;
 
-public class MockAssemblyResolver : IAssemblyResolver
+public class TestAssemblyResolver : DefaultAssemblyResolver
 {
-    public AssemblyDefinition Resolve(AssemblyNameReference name)
-    {
-        var fileName = Path.Combine(Directory, name.Name) + ".dll";
-        if (File.Exists(fileName))
-        {
-            return AssemblyDefinition.ReadAssembly(fileName);
-        }
-        return Resolve(name.Name);
+    public TestAssemblyResolver(string searchDirectory) {
+        AddSearchDirectory(searchDirectory);
     }
-
-    public AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
-    {
-        throw new NotImplementedException();
-    }
-
-    AssemblyDefinition Resolve(string fullName)
-    {
-        string codeBase;
-        if (fullName == "System")
-        {
-            codeBase = typeof(GeneratedCodeAttribute).Assembly.CodeBase;
-        }
-        else
-        {
-            codeBase = Assembly.Load(fullName).CodeBase;
-        }
-
-        var file = codeBase.Replace("file:///", "");
-        return AssemblyDefinition.ReadAssembly(file);
-    }
-
-    public void Dispose()
-    {
-    }
-
-    public string Directory;
 }
