75e08c2cae02e38256fad40e6756968b036c1751
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2019-07-05 15:33:34 +0200

    fixing tests (added property to wrong class)

--- AssemblyToProcess/DoNotReplaceEqualityOperators.cs
+++ AssemblyToProcess/DoNotReplaceEqualityOperators.cs
@@ -1,6 +1,15 @@
-﻿[Equals(DoNotAddEqualityOperators = true)]
+﻿using System;
+
+[Equals(DoNotAddEqualityOperators = true)]
 public class DoNotReplaceEqualityOperators
 {
+    public DoNotReplaceEqualityOperators()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; }
+
     public static bool operator ==(DoNotReplaceEqualityOperators left, DoNotReplaceEqualityOperators right) => true;
     public static bool operator !=(DoNotReplaceEqualityOperators left, DoNotReplaceEqualityOperators right) => true;
 }
--- AssemblyToProcess/EmptyClass.cs
+++ AssemblyToProcess/EmptyClass.cs
@@ -1,15 +1,6 @@
-﻿using System;
-
-[Equals]
+﻿[Equals]
 public class EmptyClass
 {
-    public EmptyClass()
-    {
-        Id = Guid.NewGuid();
-    }
-
-    public Guid Id { get; }
-
     public static bool operator ==(EmptyClass left, EmptyClass right) => Operator.Weave();
     public static bool operator !=(EmptyClass left, EmptyClass right) => Operator.Weave();
 }
