977fc1211aee3e67de8de55381105cab4b3b768c
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2019-07-05 16:24:34 +0200

    remove namespace

--- Tests/BadCaseIntegrationTests.cs
+++ Tests/BadCaseIntegrationTests.cs
@@ -1,81 +1,78 @@
 ﻿using Fody;
 using Xunit;
 
-namespace Tests
+public class OperatorBadCaseIntegrationTests
 {
-    public class OperatorBadCaseIntegrationTests
+    private readonly ModuleWeaver weavingTask;
+
+    public OperatorBadCaseIntegrationTests()
     {
-        private readonly ModuleWeaver weavingTask;
+        weavingTask = new ModuleWeaver();
+    }
 
-        public OperatorBadCaseIntegrationTests()
-        {
-            weavingTask = new ModuleWeaver();
-        }
+    [Fact]
+    public void ClassWithoutOperatorsOptOutButWeavingInstruction()
+    {
+        var exception = Assert.Throws<WeavingException>(
+            () => weavingTask.ExecuteTestRun("ClassWithOperatorsOptOutButWeavingInstruction.dll"));
 
-        [Fact]
-        public void ClassWithoutOperatorsOptOutButWeavingInstruction()
-        {
-            var exception = Assert.Throws<WeavingException>(
-                () => weavingTask.ExecuteTestRun("ClassWithOperatorsOptOutButWeavingInstruction.dll"));
+        Assert.Equal(
+            "Type OperatorsOptOutButHasWeavingInstruction marked with [Equals(DoNotAddEqualityOperators = true)] contains op_Equality with the instruction to weave it. Either set `DoNotAddEqualityOperators` to `false` or implement the operator properly.",
+            exception.Message);
+    }
 
-            Assert.Equal(
-                "Type OperatorsOptOutButHasWeavingInstruction marked with [Equals(DoNotAddEqualityOperators = true)] contains op_Equality with the instruction to weave it. Either set `DoNotAddEqualityOperators` to `false` or implement the operator properly.",
-                exception.Message);
-        }
+    [Fact]
+    public void StructWithoutOperatorsOptOutButWeavingInstruction()
+    {
+        var exception = Assert.Throws<WeavingException>(
+            () => weavingTask.ExecuteTestRun("StructWithOperatorsOptOutButWeavingInstruction.dll"));
 
-        [Fact]
-        public void StructWithoutOperatorsOptOutButWeavingInstruction()
-        {
-            var exception = Assert.Throws<WeavingException>(
-                () => weavingTask.ExecuteTestRun("StructWithOperatorsOptOutButWeavingInstruction.dll"));
+        Assert.Equal(
+            "Type OperatorsOptOutButHasWeavingInstruction marked with [Equals(DoNotAddEqualityOperators = true)] contains op_Equality with the instruction to weave it. Either set `DoNotAddEqualityOperators` to `false` or implement the operator properly.",
+            exception.Message);
+    }
 
-            Assert.Equal(
-                "Type OperatorsOptOutButHasWeavingInstruction marked with [Equals(DoNotAddEqualityOperators = true)] contains op_Equality with the instruction to weave it. Either set `DoNotAddEqualityOperators` to `false` or implement the operator properly.",
-                exception.Message);
-        }
+    [Fact]
+    public void ClassWithoutOperators()
+    {
+        var exception = Assert.Throws<WeavingException>(
+            () => weavingTask.ExecuteTestRun("ClassWithoutOperators.dll"));
 
-        [Fact]
-        public void ClassWithoutOperators()
-        {
-            var exception = Assert.Throws<WeavingException>(
-                () => weavingTask.ExecuteTestRun("ClassWithoutOperators.dll"));
+        Assert.Equal(
+            "Type WithoutOperators marked with the [Equals] attribute does not contain op_Equality. Fix this by adding a method `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
+            exception.Message);
+    }
 
-            Assert.Equal(
-                "Type WithoutOperators marked with the [Equals] attribute does not contain op_Equality. Fix this by adding a method `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
-                exception.Message);
-        }
+    [Fact]
+    public void StructWithoutOperators()
+    {
+        var exception = Assert.Throws<WeavingException>(
+            () => weavingTask.ExecuteTestRun("StructWithoutOperators.dll"));
 
-        [Fact]
-        public void StructWithoutOperators()
-        {
-            var exception = Assert.Throws<WeavingException>(
-                () => weavingTask.ExecuteTestRun("StructWithoutOperators.dll"));
+        Assert.Equal(
+            "Type WithoutOperators marked with the [Equals] attribute does not contain op_Equality. Fix this by adding a method `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
+            exception.Message);
+    }
 
-            Assert.Equal(
-                "Type WithoutOperators marked with the [Equals] attribute does not contain op_Equality. Fix this by adding a method `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
-                exception.Message);
-        }
+    [Fact]
+    public void ClassWithoutWeavingInstruction()
+    {
+        var exception = Assert.Throws<WeavingException>(
+            () => weavingTask.ExecuteTestRun("ClassWithoutWeavingInstruction.dll"));
 
-        [Fact]
-        public void ClassWithoutWeavingInstruction()
-        {
-            var exception = Assert.Throws<WeavingException>(
-                () => weavingTask.ExecuteTestRun("ClassWithoutWeavingInstruction.dll"));
+        Assert.Equal(
+            "TType WithoutWeavingInstruction marked with the [Equals] attribute contains op_Equality, but it does not contain the instruction to weave it. Either set implement the method like `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
+            exception.Message);
+    }
 
-            Assert.Equal(
-                "TType WithoutWeavingInstruction marked with the [Equals] attribute contains op_Equality, but it does not contain the instruction to weave it. Either set implement the method like `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
-                exception.Message);
-        }
+    [Fact]
+    public void StructWithoutWeavingInstruction()
+    {
+        var exception = Assert.Throws<WeavingException>(
+            () => weavingTask.ExecuteTestRun("StructWithoutWeavingInstruction.dll"));
 
-        [Fact]
-        public void StructWithoutWeavingInstruction()
-        {
-            var exception = Assert.Throws<WeavingException>(
-                () => weavingTask.ExecuteTestRun("StructWithoutWeavingInstruction.dll"));
-
-            Assert.Equal(
-                "TType WithoutWeavingInstruction marked with the [Equals] attribute contains op_Equality, but it does not contain the instruction to weave it. Either set implement the method like `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
-                exception.Message);
-        }
+        Assert.Equal(
+            "TType WithoutWeavingInstruction marked with the [Equals] attribute contains op_Equality, but it does not contain the instruction to weave it. Either set implement the method like `public static bool operator ==(T left, T right) => Operator.Weave();` or, if you don't want the operator to be woven: set `[Equals].DoNotAddEqualityOperators = true`.",
+            exception.Message);
     }
 }
