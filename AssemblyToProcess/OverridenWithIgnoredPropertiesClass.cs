1f300e8c94ef7fbb6644bd4a456558d2b1b50b78
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2019-07-08 08:11:59 +0200

    Removed examples for left-over method-references to weave instruction, causing PEVerify errors.

--- AssemblyToProcess/OverridenWithIgnoredPropertiesClass.cs
+++ AssemblyToProcess/OverridenWithIgnoredPropertiesClass.cs
@@ -13,15 +13,4 @@ public class ProjectBaseClass
     public virtual string Location { get; set; }
 
     public int X { get; set; }
-
-    // TODO: adding the following code here leads to a PEVerify problem. It shouldn't (but should throw an exception at runtime instead!)
-    //public static bool operator ==(ProjectBaseClass left, ProjectBaseClass right) => Operator.Weave();
-    //public static bool operator !=(ProjectBaseClass left, ProjectBaseClass right) => Operator.Weave();
-}
-
-// TODO: issue above is reproducible with class that's not been inherited from and which does not have [Equals] attribute.
-public class FooBar
-{
-    public static bool operator ==(FooBar left, FooBar right) => Operator.Weave();
-    public static bool operator !=(FooBar left, FooBar right) => Operator.Weave();
 }
