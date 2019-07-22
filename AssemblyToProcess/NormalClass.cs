9782c5058fcb0b4f00e00d96c64119c1a3d51d02
Juchli Bruno Lorenz <bruno.lorenz.juchli@erowa.com>
2019-07-22 14:34:04 +0200

    repro for #99

--- AssemblyToProcess/NormalClass.cs
+++ AssemblyToProcess/NormalClass.cs
@@ -1,4 +1,4 @@
-﻿[Equals]
+﻿[Equals(DoNotAddEqualityOperators = true)]
 public class NormalClass
 {
     public int X { get; set; }
@@ -11,4 +11,24 @@ public class NormalClass
 
     public static bool operator ==(NormalClass left, NormalClass right) => Operator.Weave();
     public static bool operator !=(NormalClass left, NormalClass right) => Operator.Weave();
+}
+
+/// Reproducing https://github.com/Fody/Equals/issues/99
+public class UsingNormalClass
+{
+    public bool CompareNormalClassForEquality()
+    {
+        var left = new NormalClass();
+        var right = new NormalClass();
+
+        return left == right;
+    }
+
+    public bool CompareNormalClassForInequality()
+    {
+        var left = new NormalClass();
+        var right = new NormalClass();
+
+        return left != right;
+    }
 }
