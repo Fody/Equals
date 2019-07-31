13d47567675e57e4df28cbc9715c3d055ef88504
Juchli Bruno Lorenz <bruno.lorenz.juchli@erowa.com>
2019-07-31 10:34:21 +0200

    improved test

--- Tests/AssemblyWithoutReferenceTests.cs
+++ Tests/AssemblyWithoutReferenceTests.cs
@@ -6,6 +6,8 @@ public class AssemblyWithoutReferenceTests
     [Fact]
     public void When_assembly_to_weave_does_not_reference_equals_assembly_weaving_should_not_fail()
     {
-        var exception = Assert.Throws<WeavingException>(() => new ModuleWeaver().ExecuteTestRun("AssemblyToProcessWithoutReference.dll"));
+        var testResult = new ModuleWeaver().ExecuteTestRun("AssemblyToProcessWithoutReference.dll");
+
+        Assert.NotNull(testResult.GetInstance("Foo"));
     }
 }
