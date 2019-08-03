6c46f20fb4ab6fd3d87753d4b4f1a406789b069b
Bruno Juchli <bruno.juchli@innocreative.ch>
2019-08-03 09:23:50 +0200

    Fix #103: When Equals assembly is unused build fails with an exception (#104)

---
+++ Tests/AssemblyWithoutReferenceTests.cs
@@ -0,0 +1,13 @@
+﻿using Fody;
+using Xunit;
+
+public class AssemblyWithoutReferenceTests
+{
+    [Fact]
+    public void When_assembly_to_weave_does_not_reference_equals_assembly_weaving_should_not_fail()
+    {
+        var testResult = new ModuleWeaver().ExecuteTestRun("AssemblyToProcessWithoutReference.dll");
+
+        Assert.NotNull(testResult.GetInstance("Foo"));
+    }
+}
